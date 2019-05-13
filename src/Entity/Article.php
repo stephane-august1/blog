4e44b69a0e1ecb6656946c376dc17f2a1e6bd11f
@ugust1 <44612386+stephane-august1@users.noreply.github.com>
2019-05-13 22:38:35 +0200

    Add files via upload
    add relation ManyToOne

--- src/Entity/Article.php
+++ src/Entity/Article.php
@@ -21,6 +21,12 @@ class Article
      */
     private $content;
 
+    /**
+     * @ORM\ManyToOne(targetEntity="App\Entity\Category")
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $category;
+
     public function getId(): ?int
     {
         return $this->id;
@@ -34,6 +40,18 @@ class Article
     public function setContent(string $content): self
     {
         $this->content = $content;
+
+        return $this;
+    }
+
+    public function getCategory(): ?Category
+    {
+        return $this->category;
+    }
+
+    public function setCategory(?Category $category): self
+    {
+        $this->category = $category;
 
         return $this;
     }
