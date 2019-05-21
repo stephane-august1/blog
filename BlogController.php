b0a6d36b65415a50b1481aecd96577a71d089317
@ugust1 <44612386+stephane-august1@users.noreply.github.com>
2019-05-21 11:42:12 +0200

    Add files via upload
    quetes symfony 8

---
+++ BlogController.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Category;
+use App\Entity\Article;
+
+class BlogController extends AbstractController
+{
+    /**
+     * @Route("/blog", name="index")
+     */
+    public function index()
+    {
+        return $this->render('blog/index.html.twig', [
+            'owner' => 'Stephane',
+        ]);
+    }
+    /**
+     * @Route("/blog/show/{variable}",defaults={"variable"="Article Sans Titre"},
+     * requirements={"variable"="[a-z0-9-]+"},name="show")
+     */
+    public function show($variable)
+    {
+        $variable = ucwords($variable, "-");
+        $var = str_replace("-", " ", $variable);
+
+        $repo = $this->getDoctrine()->getRepository(Article::class);
+        $article=$repo->findBy(array(), array('id' => 'DESC'), 3);
+       
+        return $this->render('blog/show.html.twig', [
+            'slug' => $var ,
+            'article' => $article,
+            
+
+            
+            
+        ]);
+    }
+
+    /**
+         * @Route("/article", name="article")
+         */
+    public function article()
+    {
+        $repo = $this->getDoctrine()->getRepository(Article::class);//creer une var repo lire article avec doctrine
+        $articles = $repo->findAll();  //trouve tt les articles
+
+        return $this->render('blog/article.html.twig', [
+           
+            'articles' => $articles   //lier article au twig
+        ]);
+    }
+
+
+
+    /**
+    * @Route("/category/{categoryName}",name="category")
+    */
+    
+    public function showByCategory(string $categoryName)
+    { 
+        //1 er methode avec findby et findOneby
+        /*
+         $cat = $this->getDoctrine()->getRepository(Category::class)->findOneby(['title'=> $categoryName]);
+         $articles = $this->getDoctrine()->getRepository(Article::class)->findBy(array('category' => $cat), array('id' => 'DESC'), 3);
+        */
+
+        //2em Methode par getArticles
+         $cat = $this->getDoctrine()->getRepository(Category::class)->findOneby(['title'=> $categoryName]);
+       
+        $articles = $cat->getArticles();
+
+        return $this->render('blog/category.html.twig', [
+            'category' => $categoryName ,
+            'article' => $articles,
+        ]);
+    }
+}
